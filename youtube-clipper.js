d23d37b879dd2110aedda3fe6a9d98eab2bc64ee
Christian Testa <christian.testa@tufts.edu>
2016-01-05 20:41:28 -0500

    YouTube link parsing added to youtube-clipper.js

---
+++ youtube-clipper.js
@@ -0,0 +1,12 @@
+function YouTubeClip() {
+    var youtube_id = youtube_parser( document.getElementById("startURL").value );
+}
+
+// Function taken from the following stackoverflow comment
+// http://stackoverflow.com/a/8260383/3161979
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
