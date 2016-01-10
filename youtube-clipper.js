6f008759556fe6a2181ba23464405a77cbbbb9d7
Christian Testa <testa101@gmail.com>
2016-01-09 18:14:59 -0600

    Added embed code generation and smarter conditionals for creating YouTube clip URL

--- youtube-clipper.js
+++ youtube-clipper.js
@@ -1,7 +1,14 @@
 function YouTubeClip() {
-  // Multiple condition testing style taken from the following stackoverflow comment
-  // http://stackoverflow.com/a/19478159/3161979
-  //
+
+  var finalURL = "http://youtube.com/v/";
+  var youtube_id = youtube_parser(document.getElementById("startURL").value);
+  var startsec = parseInt(document.getElementById("startsec").value);
+  var startmin = parseInt(document.getElementById("startmin").value);
+  var endsec = parseInt(document.getElementById("endsec").value);
+  var endmin = parseInt(document.getElementById("endmin").value);
+  var starttime = startmin * 60 + startsec;
+  var endtime = endmin * 60 + endsec;
+  
   // condition0: URL has a valid YouTube video ID
   // condition1: startsec is valid second value
   // condition2: startmin is valid minute value
@@ -9,42 +16,53 @@ function YouTubeClip() {
   // condition4: endmin is valid minute value 
   // condition5: end time is greater than start time
   // condition6: URL is valid YouTube URL
-  var a = 0;
-  var b = 0; 
-  var youtube_id = youtube_parser(document.getElementById("startURL").value);
-  var startsec = parseInt(document.getElementById("startsec").value);
-  var startmin = parseInt(document.getElementById("startmin").value);
-  var endsec = parseInt(document.getElementById("endsec").value);
-  var endmin = parseInt(document.getElementById("endmin").value);
   var condition0 = youtube_id;
   var condition1 = validator.isInt(startsec, {min: 0, max:59 });
   var condition2 = validator.isInt(startmin, {min: 0, max:59 });
   var condition3 = validator.isInt(endsec, {min: 0, max:59 });
   var condition4 = validator.isInt(endmin, {min: 0, max:59 });
-  var condition5 = endmin * 60 + endsec >= startmin * 60 + endsec;
+  var condition5 = endtime > starttime;
   var condition6 = validator.isURL(document.getElementById("startURL").value, {
     protocols: ['http','https'], 
     host_whitelist: ["www.youtube.com", "youtube.com", "www.youtu.be", "youtu.be"]
   });
-  a += (condition0)? 1 : 0; b += 1;
-  a += (condition1)? 1 : 0; b += 1;
-  a += (condition2)? 1 : 0; b += 1;
-  a += (condition6)? 1 : 0; b += 1;
-  if (a == b) {
-    if (condition3 && condition4 && condition5) {
-      // Both valid start and end times provided
-      var starttime = startmin * 60 + startsec;
-      var endtime = endmin * 60 + endmin;
-      // URL construction formatted based on the following stackoverflow comment:
-      // http://webapps.stackexchange.com/a/61398
-      var finalURL = "https://youtube.com/v/" + youtube_id + 
-        "&start=" + starttime + "&end=" + endtime + "&autoplay=1";
-      document.getElementById("newURL").value = finalURL;
-    } else {
-      // Only valid start time provided
+
+  // If valid YouTube URL (Valid URL, 
+  // URL contains youtube host, and contains video ID), 
+  // then proceed. Otherwise, error.
+  //
+  // If valid start time, 
+  // append &start=starttime
+  //
+  // If valid start time and valid end time (greater than start time),
+  // append &start=starttime&end=endtime
+  //
+  // If invalid start time and valid end time,
+  // append &end=endtime
+  //
+  // Finally, append &autoplay=1, format embed code,
+  // and display results on screen
+  if (condition0 && condition6) {
+    finalURL += youtube_id;
+    if (condition1 && condition2) {
+        finalURL += "&start=" + starttime;
+        if (condition3 && condition4) {
+            if (condition5) {
+            endtime = endmin * 60 + endsec;
+            finalURL += "&end=" + endtime;
+            } else alert("End time not greater than start time!");
+        } 
+    } else if (condition3 && condition4) {
+        finalURL += "&end=" + endtime;
     }
+    finalURL += "&autoplay=1";
+    finalEmbed = "<iframe src=\"";
+    finalEmbed += finalURL;
+    finalEmbed += "\" frameborder=\"0\" allowfullscreen></iframe>"
+    document.getElementById("newURL").value = finalURL;
+    document.getElementById("newEmbed").value = finalEmbed;
   } else {
-    // error
+    alert("Invalid URL provided: YouTube Clipper only works with YouTube video URLs");
   }
 }
 
